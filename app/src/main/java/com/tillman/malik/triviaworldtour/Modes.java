231182e7a57f8c2cfe63002f7a0464f1f0f4f2cb
malik-tillman <contact@maliktillman.com>
2019-02-25 11:51:43 -0500

    Scale now uses setLayoutParams instead of setScale[X/Y]

--- app/src/main/java/com/tillman/malik/triviaworldtour/Modes.java
+++ app/src/main/java/com/tillman/malik/triviaworldtour/Modes.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
@@ -94,10 +95,9 @@ public class Modes extends Fragment {
             );
 
             /*Set Gif Scale*/
-            dfdf
-            gifView.setScaleX(scaleFactor);
-            gifView.setScaleY(scaleFactor);
-
+            gifView.setLayoutParams(
+                     new LinearLayout.LayoutParams(250,250)
+            );
         }
 
 
